fa9d13d91a37d37ee8498e7372c6d0b91c2bf693
Henri Golo <henri.golo.313f@gmail.com>
2026-03-27 09:45:47 +0100

    moreteapots

--- TP12/moreteapots.cpp
+++ TP12/moreteapots.cpp
@@ -29,37 +29,37 @@ void display()
     glMatrixMode(GL_MODELVIEW);
 
     // draw scene
-
-    // add a copy of the curr. matrix to the stack
-    glPushMatrix();
-
-    glPushMatrix();
-    // translate by -3 on the z
-    glTranslatef(.0f, .0f, -3.f);
-    // set drawing color to red
-    glColor3f(1.0f, 0.0f, 0.0f);
-    // middle red teapot
-    glutWireTeapot(1);
-    // translate by 2 on the y
-    glTranslatef(.0f, 2.f, .0f);
-    // set drawing color to green
-    glColor3f(.0f, 1.0f, .0f);
-    // rotate 90 deg around x
-    glRotatef(90.f, 1.0f, 0.0f, 0.0f);
-    // top green teapot
-    glutWireTeapot(1);
-    // pop the current matrix
-    glPopMatrix();
-
-    // translate -2 on y and -1 on z
-    glTranslatef(.0f, -2.f, -1.f);
-    // set drawing color to blue
-    glColor3f(.0f, .0f, 1.f);
-    // bottom blue teapot
-    glutWireTeapot(1);
-
-    // pop the current matrix
-    glPopMatrix();
+    {
+        // add a copy of the curr. matrix to the stack
+        glPushMatrix();
+        {
+            glPushMatrix();
+            // translate by -3 on the z
+            glTranslatef(.0f, .0f, -3.f);
+            // set drawing color to red
+            glColor3f(1.0f, 0.0f, 0.0f);
+            // middle red teapot
+            glutWireTeapot(1);
+            // translate by 2 on the y
+            glTranslatef(.0f, 2.f, .0f);
+            // set drawing color to green
+            glColor3f(.0f, 1.0f, .0f);
+            // rotate 90 deg around x
+            glRotatef(90.f, 1.0f, 0.0f, 0.0f);
+            // top green teapot
+            glutWireTeapot(1);
+            // pop the current matrix
+            glPopMatrix();
+        }
+        // translate -2 on y and -1 on z
+        glTranslatef(.0f, -2.f, -1.f);
+        // set drawing color to blue
+        glColor3f(.0f, .0f, 1.f);
+        // bottom blue teapot
+        glutWireTeapot(1);
+        // pop the current matrix
+        glPopMatrix();
+    }
 
     // flush drawing routines to the window
     glFlush();
